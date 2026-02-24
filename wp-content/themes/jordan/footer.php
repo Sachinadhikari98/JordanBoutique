2b4c3f4c3a3b719c0f9e60f73be166e69eb0f56d
Sachin <sachinadh150@gmail.com>
2026-02-24 07:39:05 +0545

    new commit

--- wp-content/themes/jordan/footer.php
+++ wp-content/themes/jordan/footer.php
@@ -24,7 +24,7 @@
 <h4 class="font-bold uppercase text-xs tracking-widest mb-6 text-zinc-400">Boutique</h4>
 <ul class="space-y-4 text-sm font-medium">
 <li><a class="hover:text-primary transition-colors" href="<?php echo get_permalink(get_page_by_path('about')); ?>">Our Story</a></li>
-<li><a class="hover:text-primary transition-colors" href="<?php echo get_permalink(get_page_by_path('locations')); ?>">Store Locations</a></li>
+<li><a class="hover:text-primary transition-colors" href="<?php echo get_permalink(get_page_by_path('contact')); ?>">Store Locations</a></li>
 <li><a class="hover:text-primary transition-colors" href="<?php echo get_permalink(get_page_by_path('privacy-policy')); ?>">Privacy Policy</a></li>
 </ul>
 </div>
